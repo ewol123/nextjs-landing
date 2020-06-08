522e4a761a58875cca7ad3cecd4abf6a6d847d1d
Gyulavári Peti <gypeti23@gmail.com>
2020-06-08 21:54:12 +0200

    feat: add generic browserstack tests

---
+++ tests/local/heading_test.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'headingSegment test' : function (browser) {
+      browser
+        .url('http://localhost:3000/')
+        .waitForElementVisible('body', 1000)
+        .assert.containsText('body', 'Chat everywhere')
+        .end();
+    }
+  };
